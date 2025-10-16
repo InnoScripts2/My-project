4955aa0a8e9b7efa51805310e19238ea6003e041
ngorodinskij5 <ngorodinskij5@gmail.com>
2025-10-16 14:34:04 +0900

    chore(repo): синхронизировать текущий снимок

---
+++ apps/android-kiosk/app/build/generated/source/buildConfig/debug/com/selfservice/kiosk/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.selfservice.kiosk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.selfservice.kiosk.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0";
+  // Field from build type: debug
+  public static final boolean DEV_SKIP_ENABLED = true;
+  // Field from default config.
+  public static final String KINGBOLEN_ADAPTER_ADDRESS = "";
+  // Field from build type: debug
+  public static final int THICKNESS_REPORT_RETENTION = 50;
+}
